b269db878d0d969a7cce93144f1975e256f528d6
Sunil kumar <sunilmsdcsk@gmail.com>
2026-02-10 21:41:24 +0530

    Refactor index.html for new dashboard layout update

--- Dashboard.jsx
+++ Dashboard.jsx
@@ -1,1 +1,12 @@
+import React from "react";
 
+export default function Dashboard({ subjects, courses, batches, students }) {
+  return (
+    <div className="grid">
+      <div className="card zoom">Subjects<br />{subjects.length}</div>
+      <div className="card zoom">Courses<br />{courses.length}</div>
+      <div className="card zoom">Batches<br />{batches.length}</div>
+      <div className="card zoom">Students<br />{students.length}</div>
+    </div>
+  );
+}
